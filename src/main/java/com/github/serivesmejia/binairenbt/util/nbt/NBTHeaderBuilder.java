7ab4e5405170eb8826a65486b4c653b67af2e46a
serivesmejia <serivesmejia@gmail.com>
2021-01-28 19:52:39 -0700

    Added docs for NBTHeaderBuilder

--- src/main/java/com/github/serivesmejia/binairenbt/util/nbt/NBTHeaderBuilder.java
+++ src/main/java/com/github/serivesmejia/binairenbt/util/nbt/NBTHeaderBuilder.java
@@ -13,6 +13,11 @@ public class NBTHeaderBuilder {
     private byte[] nameBytes;
     private byte[] nameLengthBytes = new byte[Constants.TAG_SHORT_PAYLOAD_CAPACITY];
 
+    /**
+     * Sets the tag id for this header
+     * @param tagId the tag id
+     * @return this
+     */
     public NBTHeaderBuilder setTagId(byte tagId) {
         if(bb != null) {
             bb.position(0);
@@ -23,6 +28,13 @@ public class NBTHeaderBuilder {
         return this;
     }
 
+    /**
+     * Sets the name for this tag's header
+     * @param name the name
+     * @throws IllegalStateException if the name has already been set
+     * @throws IllegalArgumentException if the name length in bytes is bigger than an unsigned short max value
+     * @return this
+     */
     public NBTHeaderBuilder setName(String name) {
 
         if(bb != null) throw new IllegalStateException("Name has already been set");
@@ -49,6 +61,11 @@ public class NBTHeaderBuilder {
         return this;
     }
 
+    /**
+     * Builds this tag header
+     * @throws IllegalStateException if the tag's name or id hasn't been defined
+     * @return the bytes of this header
+     */
     public byte[] build() {
         if(tagId < 0)
             throw new IllegalStateException("Cannot build before setting tag id");
@@ -58,10 +75,18 @@ public class NBTHeaderBuilder {
         return bb.array();
     }
 
+    /**
+     * Get the tag's name in UTF-8 bytes
+     * @return the tag name in UTF-8 bytes
+     */
     public byte[] getNameBytes() {
         return nameBytes;
     }
 
+    /**
+     * Get the name UTF-8 bytes length, in bytes
+     * @return the name UTF-8 bytes length as byte array of an unsigned short
+     */
     public byte[] getNameLengthBytes() {
         return nameLengthBytes;
     }
